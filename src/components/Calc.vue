730aefc609c2beb473a94b5bd7b4de4d4fa6ee4f
begimov <begimov@gmail.com>
2018-04-04 17:48:09 +0300

    Calc UI updated

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,7 +1,66 @@
 <template>
   <div class="container my-5">
     <div class="row">
-      <div class="col">Золото</div>
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+
+            <div class="row">
+              <div class="col">
+                <ul class="list-inline">
+                  <li class="list-inline-item"><h3><span class="badge badge-danger">Золото</span></h3></li>
+                  <li class="list-inline-item"><h3><span class="badge badge-light"><a href="#" @click.prevent>Скупка</a></span></h3></li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="row mb-2">
+              <div class="col">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="text-danger">Вес</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row mb-2">
+              <div class="col">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="text-danger">Срок займа</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row mb-2">
+              <div class="col">
+                <div class="card">
+                  <div class="card-body">
+                    <h5>Ежедневный платёж</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card">
+                  <div class="card-body">
+                    <h5>Сумма</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card">
+                  <div class="card-body">
+                    <h5>Процентная ставка</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>

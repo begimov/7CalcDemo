d84dda074b3259a862321d919f9538208a018b2a
begimov <begimov@gmail.com>
2018-04-05 07:19:27 +0300

    appear transitions added

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -6,97 +6,111 @@
           <div class="card-body">
 
             <div class="row">
-              <div class="col text-center">
-                <ul class="list-inline">
-                  <li class="list-inline-item"><h3><span class="badge badge-danger">Золото</span></h3></li>
-                  <li class="list-inline-item"><h3><span class="badge badge-light"><a href="#" @click.prevent>Скупка</a></span></h3></li>
-                </ul>
-              </div>
+              <transition appear name="weight">
+                <div class="col text-center">
+                  <ul class="list-inline">
+                    <li class="list-inline-item"><h3><span class="badge badge-danger">Золото</span></h3></li>
+                    <li class="list-inline-item"><h3><span class="badge badge-light"><a href="#" @click.prevent>Скупка</a></span></h3></li>
+                  </ul>
+                </div>
+              </transition>
             </div>
 
             <div class="row mb-2">
               <div class="col-lg-6">
-                <div class="card h-100">
-                  <div class="card-body">
-                    <h4 class="text-danger text-center mb-0">Вес</h4>
-                    <VueSlideBar
-                      v-model="sliderWeight.value"
-                      :min="1"
-                      :max="10"
-                      :processStyle="sliderWeight.processStyle"
-                      :lineHeight="sliderWeight.lineHeight"
-                      :tooltipStyles="{ backgroundColor: 'red', borderColor: 'red' }">
-                    </VueSlideBar>
+                <transition appear name="weight">
+                  <div class="card h-100">
+                    <div class="card-body">
+                      <h4 class="text-danger text-center mb-0">Вес</h4>
+                      <VueSlideBar
+                        v-model="sliderWeight.value"
+                        :min="1"
+                        :max="10"
+                        :processStyle="sliderWeight.processStyle"
+                        :lineHeight="sliderWeight.lineHeight"
+                        :tooltipStyles="{ backgroundColor: 'red', borderColor: 'red' }">
+                      </VueSlideBar>
+                    </div>
                   </div>
-                </div>
+                </transition>
               </div>
               <div class="col-lg-6">
-                <div class="card h-100">
-                  <div class="card-body">
-                    <h4 class="text-danger text-center mb-4">Срок займа</h4>
-                    <div class="row">
-                      <div class="col text-center">
-                        <div class="form-check form-check-inline">
-                          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="31" v-model="loanTerm">
-                          <label class="form-check-label lead mt-1" for="inlineRadio1">31 день</label>
+                <transition appear name="loanterm">
+                  <div class="card h-100">
+                    <div class="card-body">
+                      <h4 class="text-danger text-center mb-4">Срок займа</h4>
+                      <div class="row">
+                        <div class="col text-center">
+                          <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="31" v-model="loanTerm">
+                            <label class="form-check-label lead mt-1" for="inlineRadio1">31 день</label>
+                          </div>
                         </div>
-                      </div>
-                      <div class="col text-center">
-                        <div class="form-check form-check-inline">
-                          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="51" v-model="loanTerm">
-                          <label class="form-check-label lead mt-1" for="inlineRadio2">51 день</label>
+                        <div class="col text-center">
+                          <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="51" v-model="loanTerm">
+                            <label class="form-check-label lead mt-1" for="inlineRadio2">51 день</label>
+                          </div>
                         </div>
-                      </div>
-                    </div>                    
+                      </div>                    
+                    </div>
                   </div>
-                </div>
+                </transition>
               </div>
             </div>
 
             <div class="row my-3">
               <div class="col">
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="text-danger text-center mb-0">Проба</h4>
-                    <VueSlideBar
-                      v-model="sliderGoldContent.value"
-                      :data="sliderGoldContent.data"
-                      :range="sliderGoldContent.range"
-                      :min="1"
-                      :max="10"
-                      :processStyle="sliderWeight.processStyle"
-                      :lineHeight="sliderWeight.lineHeight"
-                      :tooltipStyles="{ backgroundColor: 'red', borderColor: 'red' }">
-                    </VueSlideBar>
+                <transition appear name="goldcontent">
+                  <div class="card">
+                    <div class="card-body">
+                      <h4 class="text-danger text-center mb-0">Проба</h4>
+                      <VueSlideBar
+                        v-model="sliderGoldContent.value"
+                        :data="sliderGoldContent.data"
+                        :range="sliderGoldContent.range"
+                        :min="1"
+                        :max="10"
+                        :processStyle="sliderWeight.processStyle"
+                        :lineHeight="sliderWeight.lineHeight"
+                        :tooltipStyles="{ backgroundColor: 'red', borderColor: 'red' }">
+                      </VueSlideBar>
+                    </div>
                   </div>
-                </div>
+                </transition>
               </div>
             </div>
 
             <div class="row my-3">
               <div class="col-md-4">
-                <div class="card h-100 bg-dark text-white">
-                  <div class="card-body">
-                    <h5>Ежедневный платёж</h5>
-                    <p class="display-4 mb-0">{{ dailyPayment }}</p>
+                <transition appear name="dailypayment">
+                  <div class="card h-100 bg-dark text-white">
+                    <div class="card-body">
+                      <h5>Ежедневный платёж</h5>
+                      <p class="display-4 mb-0">{{ dailyPayment }}</p>
+                    </div>
                   </div>
-                </div>
+                </transition>
               </div>
               <div class="col-md-4">
-                <div class="card h-100 bg-primary text-white">
-                  <div class="card-body">
-                    <h5>Сумма</h5>
-                    <p class="display-4 mb-0">{{ total }}</p>
+                <transition appear name="total">
+                  <div class="card h-100 bg-primary text-white">
+                    <div class="card-body">
+                      <h5>Сумма</h5>
+                      <p class="display-4 mb-0">{{ total }}</p>
+                    </div>
                   </div>
-                </div>
+                </transition>
               </div>
               <div class="col-md-4">
-                <div class="card h-100 bg-danger text-white">
-                  <div class="card-body">
-                    <h5>Процентная ставка</h5>
-                    <p class="display-4 mb-0">{{ interestRate }}</p>
+                <transition appear name="interestRate">
+                  <div class="card h-100 bg-danger text-white">
+                    <div class="card-body">
+                      <h5>Процентная ставка</h5>
+                      <p class="display-4 mb-0">{{ interestRate }}</p>
+                    </div>
                   </div>
-                </div>
+                </transition>
               </div>
             </div>
 
@@ -164,5 +178,52 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.weight-enter-active, .weight-leave-active {
+  transition: opacity 1.5s;
+}
+.weight-enter, .weight-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+}
 
+.loanterm-enter-active, .loanterm-leave-active {
+  transition: opacity 1.5s .5s;
+}
+.loanterm-enter, .loanterm-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+}
+
+.goldcontent-enter-active, .goldcontent-leave-active {
+  transition: opacity 1.5s 1s;
+}
+.goldcontent-enter, .goldcontent-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+}
+
+
+.dailypayment-enter-active, .dailypayment-leave-active {
+  transition: all 1.5s ease 1.5s;
+}
+.dailypayment-enter, .dailypayment-leave-to
+/* .slide-fade-leave-active до версии 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
+.total-enter-active, .total-leave-active {
+  transition: all 1.5s ease 2s;
+}
+.total-enter, .total-leave-to
+/* .slide-fade-leave-active до версии 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
+.interestRate-enter-active, .interestRate-leave-active {
+  transition: all 1.5s ease 2.5s;
+}
+.interestRate-enter, .interestRate-leave-to
+/* .slide-fade-leave-active до версии 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
 </style>
